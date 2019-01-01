768889297227a961612a9ba360d4e3aa8e18700b
Darren Ford <dford_au-reg@yahoo.com>
2019-01-01 17:28:05 +1100

    Initial Commit

---
+++ DSFColorPicker/DSFColorPicker.h
@@ -0,0 +1,19 @@
+//
+//  DSFColorPicker.h
+//  DSFColorPicker
+//
+//  Created by Darren Ford on 1/1/19.
+//  Copyright © 2019 Darren Ford. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for DSFColorPicker.
+FOUNDATION_EXPORT double DSFColorPickerVersionNumber;
+
+//! Project version string for DSFColorPicker.
+FOUNDATION_EXPORT const unsigned char DSFColorPickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DSFColorPicker/PublicHeader.h>
+
+
